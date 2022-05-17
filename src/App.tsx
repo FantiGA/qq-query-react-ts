c4755f465a2b4c114619e5dd05c8fd1b294888e3
FantiGA <98534535+FantiGA@users.noreply.github.com>
2022-05-17 18:14:54 +0800

    * finish the base features

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,69 @@
-import React from 'react'
-import "./App.css";
+/*
+ * @Author: fantiga
+ * @Date: 2022-05-17 15:35:26
+ * @LastEditTime: 2022-05-17 18:12:09
+ * @LastEditors: fantiga
+ * @Description: 
+ * @FilePath: /react-qq-query-ts/src/App.tsx
+ */
+
+import React, { useState, useEffect } from 'react'
+import "./App.scss";
+
+import './components/Query/index.scss'
+import './components/Error/index.scss'
+import './components/Result/index.scss'
+// import Query from './components/Query'
+// import Error from './components/Error'
+// import Result from './components/Result'
+
+import axios from 'axios'
 
 export default function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  )
+    const [data, setData] = useState({ qq: '', requestQq: '', name: '', qlogo: '' })
+
+    const queryHandler = async (qq: string) => {
+        const apiUrl = 'https://api.uomg.com/api/qq.info'
+        const result = await axios.get(apiUrl, {
+            params: {
+                qq: qq
+            }
+        })
+
+        const { qq: requestQq, name, qlogo } = result.data
+        setData({ qq, requestQq, name, qlogo })
+    }
+
+    const inputHandler = (params: { qq: string }): void => {
+        const { qq } = params
+        queryHandler(qq)
+    }
+
+    return (
+        <div className='container'>
+            <div className='wrapper'>
+                <form action=''>
+                    <h1>QQ号查询</h1>
+                    <label>QQ<input
+                        className='input-qq'
+                        type='number'
+                        min='10000'
+                        placeholder='输入QQ号码'
+                        onChange={event => inputHandler({
+                            qq: event.target.value
+                        })}
+                    /></label>
+                </form>
+                <div className='loading'>Loading...</div>
+                <div className='errorMsg'>Error</div>
+                <div className='resultBox'>
+                    <img src={data.qlogo} />
+                    <dl>
+                        <dt>{data.name}</dt>
+                        <dd>{data.requestQq}</dd>
+                    </dl>
+                </div>
+            </div>
+        </div>
+    )
 }
---
+++ src/components/Query/index.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: fantiga
+ * @Date: 2022-05-17 09:39:06
+ * @LastEditTime: 2022-05-17 15:31:08
+ * @LastEditors: fantiga
+ * @Description: 
+ * @FilePath: /react-qq-query-ts/src/components/Query/index.tsx
+ */
+
+import React from 'react'
+import './index.scss'
+
+export default function Query() {
+    return (
+        <div>Query</div>
+    )
+}
